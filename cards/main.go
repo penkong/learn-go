bd5631b26f12f2064d82e9b822ed5346f728d677
mazda <nazemi.works@gmail.com>
2020-12-26 18:55:07 +0000

    cool go

--- cards/main.go
+++ cards/main.go
@@ -1,2 +1,38 @@
 package main
 
+
+
+
+
+func main(){
+	// var card string = "Ace of space"
+
+	// card := newCard()
+
+	// reassign
+	// card = "Five of Diamonds"
+
+	// fmt.Println(card)
+
+	// slice
+	// cards := deck{newCard(), newCard(), "sth"}
+
+	// add to slice 
+	// cards = append(cards, "Six of sth")
+
+	// loop over slice 
+	// for i, card := range cards {
+	// 	fmt.Println(i,card)
+	// }
+
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
+}
+
+
+
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
