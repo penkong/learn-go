8eb69edf3f9159bf11f5e2edb1bc6a5280fc4c36
mazda <nazemi.works@gmail.com>
2020-12-28 20:42:47 +0000

    through it

--- go2/hello/main.go
+++ go2/hello/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hellow")
+	fmt.Println("hello, everyone ")
+	foo()
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	boo()
+}
+
+func foo() {
+	fmt.Println("i am in foo")
+}
+
+func boo() {
+	fmt.Println("exited")
 }
