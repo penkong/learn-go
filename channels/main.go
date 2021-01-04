d5b1b700dc8b62e25f46eeb7244b12c54f103795
mazda <nazemi.works@gmail.com>
2021-01-04 07:13:05 +0000

    more on

--- channels/main.go
+++ channels/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"runtime"
+	"time"
+	// "sync"
 )
 
-var wg sync.WaitGroup
+// var wg sync.WaitGroup
 
 func main() {
 	links := []string{
@@ -17,21 +19,36 @@ func main() {
 		"http://amazon.com",
 	}
 
-	wg.Add(len(links))
+	// wg.Add(len(links))
+
+	c := make(chan string)
 
 	for _, v := range links {
-		go requester(v)
+		go requester(v, c)
 	}
 
-	wg.Wait()
+	// wg.Wait()
+	// for i := 0 ; i < len(links); i++ {
+	// 	go requester(<-c , c)
+	// }
+
+	for l := range c {
+		go func(s string) {
+			time.Sleep(4 * time.Second)
+			requester(s, c)
+		}(l)
+		fmt.Println(runtime.NumGoroutine())
+	}
 }
 
-func requester(l string) {
+func requester(l string, c chan string) {
 	_, err := http.Get(l)
 	if err != nil {
 		fmt.Println(err, l)
+		c <- l
 		return
 	}
 	fmt.Println(l, "is up")
-	wg.Done()
+	c <- l
+	// wg.Done()
 }
